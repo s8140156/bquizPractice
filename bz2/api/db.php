ac7b79a639d7b0881082298462e12b2d27b8b8dd
s8140156 <s8140156@gmail.com>
2024-02-04 01:22:08 +0800

    練習後台新增問卷頁面及存入問卷名稱＋選項subject_id邏輯 完成step14

--- bz2/api/db.php
+++ bz2/api/db.php
@@ -131,6 +131,7 @@ function to($url){
 $Total=new DB('total');
 $User=new DB('user');
 $News=new DB('news');
+$Que=new DB('que');
 
 if(!isset($_SESSION['visited'])){
     if($Total->count(['date'=>date("Y-m-d")])>0){
---
+++ bz2/back/que.php
@@ -0,0 +1,28 @@
+<fieldset>
+    <legend>新增問卷</legend>
+    <form action="./api/add_que.php" method="post">
+        <div>
+            <div>問卷名稱
+                <input type="text" name="subject">
+            </div>
+        </div>
+        <div class="clo">
+            <div id="opt">選項
+                <input type="text" name="option[]">
+                <input type="button" value="更多" onclick="more()">
+            </div>
+        </div>
+        <div>
+            <input type="submit" value="新增">|<input type="reset" value="清空">
+        </div>
+    </form>
+</fieldset>
+<script>
+    function more(){
+        let opt=`
+        <div id="opt">選項
+                <input type="text" name="option[]">
+        </div>`;
+        $('#opt').before(opt);
+    }
+</script>
