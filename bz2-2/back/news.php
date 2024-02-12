546715c593f2062a8a6dbe36c375d169c417ecd3
s8140156 <s8140156@gmail.com>
2024-02-12 15:35:55 +0800

    練習最新文章畫面及分頁邏輯 頁碼邏輯及api串接刪除及顯示邏輯順序

---
+++ bz2-2/back/news.php
@@ -0,0 +1,48 @@
+<form action="./api/edit_news.php" method="post">
+    <table style="width:80%;text-align:center">
+        <tr>
+            <td>編號</td>
+            <td style="width:70%">標題</td>
+            <td>顯示</td>
+            <td>刪除</td>
+        </tr>
+        <?php
+        $total=$News->count();
+        $div=3;
+        $pages=ceil($total/$div);
+        $now=$_GET['p']??1;
+        $start=($now-1)*$div;
+        $rows=$News->all(" limit $start,$div");
+        foreach($rows as $idx=>$row){
+        ?>
+        <tr>
+            <td class="clo"><?=$idx+1+$start;?></td>
+            <td><?=$row['title'];?></td>
+            <td><input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>></td>
+            <td>
+                <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+            </td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <div class="ct">
+        <?php
+        if($now-1>0){
+            $prev=$now-1;
+            echo "<a href='back.php?do=news&p=$prev'> < </a>";
+        }
+        for($i=1;$i<=$pages;$i++){
+            $size=($now==$i)?'font-size:22px':'font-size:16px';
+            echo "<a href='back.php?do=news&p=$i' style='$size'> $i </a>";
+        }
+        if($now+1<=$pages){
+            $next=$now+1;
+            echo "<a href='back.php?do=news&p=$next'> > </a>";
+        }
+        ?>
+        <div class="ct"><input type="submit" value="確定修改"></div>
+    </div>
+</form>
