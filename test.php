d5f4ce1ca19c9d6a3f211b1aa62a2eafda0c5097
s8140156 <s8140156@gmail.com>
2024-02-25 23:27:28 +0800

    練習題組二第四遍 整理素材確認版面及切版 及完成db及測試功能正常

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once "db.php";
+
+// $res=$Alto->q("INSERT INTO `alto`(`total`,`text`,`sh`) VALUES ('15','希望順利','1')");
+// $res=$Alto->find(2);
+// $res=$Alto->all();
+// dd($res);
+
+// $Alto->save(['total'=>100,'text'=>'要成功啊','sh'=>'0']);
+$Alto->save(['id'=>'1','text'=>'拜託再一次成功','sh'=>'0']);
+
+
+
+
+
+
+?>
