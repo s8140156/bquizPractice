50eb7085c9f2650810906ec1a118eeb7c5c5d836
s8140156 <s8140156@gmail.com>
2024-02-25 17:42:33 +0800

    終於在人氣文章沒有搞太多時間 重點在第二欄要有relative位置設定 還有hover使用的全文設定 hover設定要去index拿記得註解原來hover格式 完成step16

--- bz2-3/index.php
+++ bz2-3/index.php
@@ -1,4 +1,18 @@
 ﻿<?php include_once "./api/db.php" ?>
+<style>
+	.pop{
+		background:rgba(51,51,51,0.8); 
+		color:#FFF; 
+		min-height:100px; 
+		width:300px; 
+		position:absolute; 
+		display:none; 
+		z-index:9999; 
+		overflow:auto;
+		padding: 15px;
+		height:400px;
+	}
+</style>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -10,9 +24,9 @@
 </head>
 
 <body>
-<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
+<!-- <div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
 	<pre id="ssaa"></pre>
-</div>
+</div> -->
 <!-- <iframe name="back" style="display:none;"></iframe> -->
 	<div id="all">
     	<div id="title">
