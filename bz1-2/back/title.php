f8171118d432ef235d9d401a2100c0fc50f89406
s8140156 <s8140156@gmail.com>
2024-02-28 20:04:55 +0800

    以back/title功能製作共用後台功能頁面及新增圖片及更新圖片彈跳視窗

--- bz1-2/back/title.php
+++ bz1-2/back/title.php
@@ -1,6 +1,6 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">網站標題管理</p>
-    <form method="post" target="back" action="?do=tii">
+    <form method="post" action="./api/edit.php"><!--注意要改成api路徑 還有target=back刪掉-->
         <table width="100%">
             <tbody>
                 <tr class="yel">
@@ -10,16 +10,31 @@
                     <td width="7%">刪除</td>
                     <td></td>
                 </tr>
+                <tr class="cent">
+                    <td width="45%"><img src="./img/" style="width:300px;height:30px"></td>
+                    <td width="23%"><input type="text" name="text[]" value=""></td>
+                    <td width="7%"><input type="radio" name="sh" value=""></td>
+                    <td width="7%"><input type="checkbox" name="del[]" value=""></td>
+                    <td><!--下面修改重要!! 更新圖片要增加op function注意路徑是在modal/upload.php並網路傳值帶table及id-->
+                        <!-- 務必記得要增加hidden id欄位 -->
+                        <input type="button" value="更新圖片" onclick="op('#cover','#cvr','./modal/upload.php?table= &id= ')">
+                        <input type="hidden" name="id[]" value="">
+                    </td>
+                </tr>
             </tbody>
         </table>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;view.php?do=title&#39;)" value="新增網站標題圖片"></td>
-                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增網站標題圖片"></td>
+                    <td class="cent">
+                        <input type="submit" value="修改確定">
+                        <input type="reset" value="重置">
+                        <input type="hidden" name="table" value="table">
+                        <!-- 這邊注意!!要增加帶hidden table欄位 name&value都是table -->
+                    </td>
                 </tr>
             </tbody>
         </table>
-
     </form>
 </div>
---
+++ bz1-2/modal/upload.php
@@ -0,0 +1,40 @@
+<?php
+switch($_GET['table']){
+    case 'title':
+        echo "<h3>更新網站標題區圖片</h3>";
+    break;
+    case 'mivm':
+        echo "<h3>更新動畫圖片</h3>";
+    break;
+    case 'image':
+        echo "<h3>更新校園映像圖片</h3>";
+    break;
+}
+?>
+<hr>
+<form action="./api/upload.php" method="post" enctype="multipart/form-data">
+    <table style="margin:auto;width:70%">
+        <tr>
+        <?php
+        switch($_GET['table']){
+        case 'title':
+            echo "<td>標題區圖片</td>";
+        break;
+        case 'mivm':
+            echo "<td>動畫圖片</td>";
+        break;
+        case 'image':
+            echo "<td>校園映像圖片</td>";
+        break;
+        }
+        ?>
+            <td><input type="file" name="img"></td>
+        </tr>
+    </table>
+    <div class="cent">
+        <input type="submit" value="更新">
+        <input type="reset" value="重置">
+        <input type="hidden" name="table" value="<?=$_GET['table'];?>">
+        <input type="hidden" name="id" value="<?=$_GET['id'];?>">
+    </div>
+</form>
