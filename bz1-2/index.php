211d438494703575c3e3dd0a8e490d356a6768a6
s8140156 <s8140156@gmail.com>
2024-02-28 17:20:20 +0800

    將頁尾版權及進站人次顯示前後台(成功)及修改後台表單路徑(改網路傳值?do)及修整多程式back-iframe+彈跳視窗

--- bz1-2/index.php
+++ bz1-2/index.php
@@ -1,3 +1,4 @@
+<?php include_once "./api/db.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -5,7 +6,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-	<title>卓越科技大學校園資訊系統</title>
+	<title>(練習2_前)卓越科技大學校園資訊系統</title>
 	<link href="./css/css.css" rel="stylesheet" type="text/css">
 	<script src="./js/jquery-1.9.1.min.js"></script>
 	<script src="./js/js.js"></script>
@@ -18,7 +19,7 @@
 			<div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
 		</div>
 	</div>
-	<iframe style="display:none;" name="back" id="back"></iframe>
+	<!-- <iframe style="display:none;" name="back" id="back"></iframe> --><!--去除iframe-->
 	<div id="main">
 		<a title="" href="./js/home.htm">
 			<div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題-->
@@ -30,8 +31,7 @@
 					<span class="t botli">主選單區</span>
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 :
-						1 </span>
+					<span class="t">進站總人數 :<?=$Total->find(4)['total'];?></span>
 				</div>
 			</div>
 			<!-- class=di start 拆去front/main -->
@@ -92,7 +92,7 @@
 		</div>
 		<div style="clear:both;"></div>
 		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-			<span class="t" style="line-height:123px;"></span>
+			<span class="t" style="line-height:123px;"><?=$Bottom->find(5)['bottom'];?></span>
 		</div>
 	</div>
 
