bc60fe4033cbd841213f1363ec80c09a4745abee
s8140156 <s8140156@gmail.com>
2024-03-01 22:50:39 +0800

    0301完成題一第二次練習 測試功能ok

--- bz1-2/index.php
+++ bz1-2/index.php
@@ -33,7 +33,7 @@
 					<!--主選單放此-->
 					<span class="t botli">主選單區</span> <!--從這邊開始寫選單表列!!!-->
 					<?php
-					$rows=$Menu->all(['sh'=>1,'menu_id'=>0]);
+					$rows=$Menu->all(['sh'=>1,'menu_id'=>0]); //主選單就是先找menu_id=0
 					foreach($rows as $row){
 					?>
 					<div class="mainmu"> <!--mainmu是既有css樣式-->
@@ -91,7 +91,16 @@
 			</script>
 			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
 				<!--右邊-->
-				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
+				<?php
+				if(isset($_SESSION['login'])){
+					$url="lo('back.php')";
+					$str="返回管理";
+				}else{
+					$url="lo('index.php?do=login')";
+					$str="管理登入";
+				}
+				?>
+				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="<?=$url;?>"><?=$str;?></button>
 				<div style="width:89%; height:480px;" class="dbor">
 					<span class="t botli">校園映象區</span>
 					<!-- 以下開始建立校園映像上下鍵及圖片放置 -->
