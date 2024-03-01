cf873caa6787b72a683216a330d08a10d1d8368d
s8140156 <s8140156@gmail>
2024-03-01 15:43:09 +0800

    前台動畫放置注意點:script程式整個要搬到動畫區html下/php寫在script裡面(非常注意echo句子要先寫完後php結束也要;/要記得先執行ww()/最重要是圖片路徑'./img/

--- bz1-2/front/main.php
+++ bz1-2/front/main.php
@@ -2,27 +2,38 @@
 <?php include "marquee.php"; ?>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
+
+    <!-- 這邊是動畫區 -->
+    <div style="width:100%; padding:2px; height:290px;">
+        <div id="mwww" loop="true" style="width:100%; height:100%;">
+            <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
+        </div>
+    </div>
+    <!-- 這邊是動畫區 -->
+    <!-- 不要忘記把script搬下來 然後動畫區的php寫在script裡面 注意!! -->
     <script>
         var lin = new Array();
+        <?php
+        $lins=$Mvim->all(['sh'=>1]);
+        foreach($lins as $lin){
+            echo "lin.push('{$lin['img']}');"; //請非常注意各種符號寫法
+        }
+        ?>
         var now = 0;
+        ww(); //這邊要執行一次 不然會三秒後才有第一張
         if (lin.length > 1) {
             setInterval("ww()", 3000);
             now = 1;
         }
 
-        function ww() {
-            $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+        function ww() { //注意!!!! 要加入img路徑拉(路徑格式請寫對)~~~~ ./img/ 要寫在 '   "這兩個中間
+            $("#mwww").html("<embed loop=true src='./img/" + lin[now] + "' style='width:99%; height:100%;'></embed>")
             //$("#mwww").attr("src",lin[now])
             now++;
             if (now >= lin.length)
                 now = 0;
         }
     </script>
-    <div style="width:100%; padding:2px; height:290px;">
-        <div id="mwww" loop="true" style="width:100%; height:100%;">
-            <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
-        </div>
-    </div>
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
         </span>
