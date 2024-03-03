9d164f96e1c09b57eca90a1c0f64c6aa667d5e98
s8140156 <s8140156@gmail.com>
2024-03-03 23:30:19 +0800

    練習商品分類輸入邏輯by ajax(這次是錯在資料表id忘記設定AI造成寫入問題)剩各商品編輯function未完成 目前step14

---
+++ bz4-1/api/save_type.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once "db.php";
+
+$Type->save($_POST);
+
+?>
---
+++ bz4-1/back/th.php
@@ -0,0 +1,83 @@
+<h2 class="ct">商品分類</h2>
+<div class="ct">新增大分類<input type="text" name="big" id="big"><button onclick="addType('big')">新增</button></div>
+<div class="ct">
+    新增中分類
+    <select name="big" id="bigs"></select>
+    <input type="text" name="mid" id="mid">
+    <button onclick="addType('mid')">新增</button>
+</div>
+<table class="all">
+    <?php
+    $bigs=$Type->all(['big_id'=>0]);
+    foreach($bigs as $big){
+    ?>
+    <tr class="tt">
+        <td><?=$big['name'];?></td>
+        <td class="ct">
+            <button onclick="edit(this,<?=$big['id'];?>)">修改</button>
+            <button onclick="del('type',<?=$big['id'];?>)">刪除</button>
+        </td>
+    </tr>
+    <?php
+    $mids=$Type->all(['big_id'=>$big['id']]);
+    foreach($mids as $mid){
+    ?>
+    <tr class="pp ct">
+        <td><?=$mid['name'];?></td>
+        <td><button onclick="edit(this,<?=$mid['id'];?>)">修改</button>
+            <button onclick="del('type',<?=$mid['id'];?>)">刪除</button>
+        </td>
+    </tr>
+    <?php  
+        } //注意這邊包法 看起來bigs迴圈裡面再包mids迴圈
+    }
+    ?>
+</table>
+<script>
+    getTypes(0);
+    function getTypes(big_id){
+        $.get('./api/get_types.php',{big_id},(types)=>{
+            $('#bigs').html(types)
+        })
+    }
+    function addType(type){
+        let name
+        let big_id
+        switch(type){
+            case 'big':
+                name=$('#big').val()
+                big_id=0;
+            break;
+            case 'mid':
+                name=$('#mid').val()
+                big_id=$('#bigs').val();
+            break;
+        }
+        $.post('./api/save_type.php',{name,big_id},()=>{
+            location.reload()
+        })
+    }
+</script>
+<h2 class="ct">商品管理</h2>
+<div class="ct"><button>新增商品</button></div>
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>庫存量</td>
+        <td>狀態</td>
+        <td>操作</td>
+    </tr>
+    <tr class="pp">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+            <button>修改</button>
+            <button>刪除</button>
+            <button>上架</button>
+            <button>下架</button>
+        </td>
+    </tr>
+</table>
