eccf9bd09c5cc0cd2518870cafb4c5f881b82244
s8140156 <s8140156@gmail.com>
2024-03-04 20:44:29 +0800

    增前台左邊選單by全部商品/大分類/中分類顯示邏輯(注意html div>a階層寫法)及增css設定調整中分類選單顏色(css格式權限設定)以利分別 完成step17

--- bz4-1/index.php
+++ bz4-1/index.php
@@ -4,78 +4,92 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-        <title>(練1_前)┌精品電子商務網站」</title>
-        <link href="./css/css.css" rel="stylesheet" type="text/css">
-        <script src="./js/js.js"></script>
-        <script src="./js/jquery-3.4.1.min.js"></script>
+    <title>(練1_前)┌精品電子商務網站」</title>
+    <link href="./css/css.css" rel="stylesheet" type="text/css">
+    <script src="./js/js.js"></script>
+    <script src="./js/jquery-3.4.1.min.js"></script>
 </head>
 
 <body>
-        <iframe name="back" style="display:none;"></iframe>
-        <div id="main">
-                <div id="top">
-                        <a href="?">
-                                <img src="./icon/0416.jpg">
-                        </a>
-                        <div style="padding:10px;">
-                                <a href="?">回首頁</a> |
-                                <a href="?do=news">最新消息</a> |
-                                <a href="?do=look">購物流程</a> |
-                                <a href="?do=buycart">購物車</a> |
-                                <?php
-                                if(isset($_SESSION['mem'])){
-                                        echo "<a href='./api/logout.php'>登出</a> |";
-                                ?>
-                                <?php
-                                }else{
-                                ?>
-                                <a href="?do=login">會員登入</a> |
-                                <?php
-                                }
-                                ?>
-                                <?php
-                                if(isset($_SESSION['admin'])){
-                                        echo "<a href='back.php'>返回管理</a>";
-                                }else{
-                                ?>
-                                <a href="?do=admin">管理登入</a>
-                                <?php
-                                }
-                                ?>
-                        </div>
-                        <marquee behavior="" direction="">情人節特惠活動 &nbsp;&nbsp; 年終特賣會開跑了</marquee>
-                </div>
-                <div id="left" class="ct">
-                        <div style="min-height:400px;">
-                        <a href=""></a>
-                        <div class="ww">
-                                <a href=""></a>
-                                <div class="s">
-                                        <a href=""></a>
-                                </div>
-                        </div>
-                        </div>
-                        <span>
-                                <div>進站總人數</div>
-                                <div style="color:#f00; font-size:28px;">
-                                        00005 </div>
-                        </span>
-                </div>
-                <div id="right">
-                        <?php
-                        $do = $_GET['do'] ?? 'main';
-                        $file = "./front/{$do}.php";
-                        if (file_exists($file)) {
-                                include $file;
-                        } else {
-                                include "./front/main.php";
-                        }
-                        ?>
-                </div>
-                <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct"><?=$Bottom->find(1)['bottom'];?></div>
+    <iframe name="back" style="display:none;"></iframe>
+    <div id="main">
+        <div id="top">
+            <a href="?">
+                <img src="./icon/0416.jpg">
+            </a>
+            <div style="padding:10px;">
+                <a href="?">回首頁</a> |
+                <a href="?do=news">最新消息</a> |
+                <a href="?do=look">購物流程</a> |
+                <a href="?do=buycart">購物車</a> |
+                <?php
+                if (isset($_SESSION['mem'])) {
+                    echo "<a href='./api/logout.php'>登出</a> |";
+                ?>
+                <?php
+                } else {
+                ?>
+                    <a href="?do=login">會員登入</a> |
+                <?php
+                }
+                ?>
+                <?php
+                if (isset($_SESSION['admin'])) {
+                    echo "<a href='back.php'>返回管理</a>";
+                } else {
+                ?>
+                    <a href="?do=admin">管理登入</a>
+                <?php
+                }
+                ?>
+            </div>
+            <marquee behavior="" direction="">情人節特惠活動 &nbsp;&nbsp; 年終特賣會開跑了</marquee>
         </div>
+        <div id="left" class="ct">
+            <div style="min-height:400px;">
+                <a href="?type=0">全部商品(<?=$Goods->count(['sh'=>1]);?>)</a>
+                <?php
+                $bigs=$Type->all(['big_id'=>0]); //這邊是在撈大分類的資料
+                foreach($bigs as $big){
+                ?>
+                <div class="ww"> <!--內建css可以hover-->
+                    <a href="?type=<?=$big['id'];?>"><?=$big['name'];?>(<?=$Goods->count(['sh'=>1,'big'=>$big['id']]);?>)</a>
+                    <div class="s">
+                        <?php
+                        if($Type->count(['big_id'=>$big['id']])>0){
+                            $mids=$Type->all(['big_id'=>$big['id']]); //這邊是在撈中分類的資料
+                            foreach($mids as $mid){
+                        ?>
+                        <a href="?type=<?=$mid['id'];?>"><?=$mid['name'];?>(<?=$Goods->count(['sh'=>1,'mid'=>$mid['id']]);?>)</a>
+                        <?php    }
+                            }
+                        ?>
+                    </div>
+                </div>
+                <?php   }
+                ?>
+            </div>
+            <span>
+                <div>進站總人數</div>
+                <div style="color:#f00; font-size:28px;">
+                    00005 </div>
+            </span>
+        </div>
+        <div id="right">
+            <?php
+            $do = $_GET['do'] ?? 'main';
+            $file = "./front/{$do}.php";
+            if (file_exists($file)) {
+                include $file;
+            } else {
+                include "./front/main.php";
+            }
+            ?>
+        </div>
+        <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct"><?= $Bottom->find(1)['bottom']; ?></div>
+    </div>
 
 </body>
 
