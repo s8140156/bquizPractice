ceb33aa875c04d667cbc98b439c4fbf1a2f3e0bc
s8140156 <s8140156@gmail.com>
2024-03-05 09:12:14 +0800

    將detail畫面完成及buy()程式 注意雖jq程式沒有太注重;但發現帶GET後面沒 有;完結 會導致無法連結下頁 完成step18

--- bz4-1/front/detail.php
+++ bz4-1/front/detail.php
@@ -1,6 +1,43 @@
 <?php
 $good=$Goods->find($_GET['id']);
 ?>
+<style>
+    .item {
+        width: 80%;
+        /* height: 160px; */
+        background-color: #f4c591;
+        margin: 5px auto;
+        display: flex;
+    }
+
+    .item .img {
+        width: 60%; /*版面照片比較大*/
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid #fff;
+        padding: 5px;
+        text-align: center;
+    }
+
+    .item .info {
+        width: 40%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .info div {
+        border: 1px solid #fff;
+        border-left: 0px;
+        border-top: 0px;
+        flex-grow: 1;
+        /*最後一個自動拉長？*/
+    }
+
+    .info div:nth-child(1) {
+        border-top: 1px solid #fff;
+    }
+</style>
 
 <h2 class="ct"><?=$good['name'];?></h2>
 
@@ -23,4 +60,11 @@ $good=$Goods->find($_GET['id']);
         <input type="number" id="qt" value="1" style="width:50px">
         <img src="./icon/0402.jpg" onclick="buy()">
     </div>
+    <script>
+        function buy(){
+            let id=<?=$_GET['id'];?>;  //好像發現如果沒有; 會導致無法點選至下頁
+            let qt=$('#qt').val()
+            location.href=`?do=buycart&id=${id}&qt=${qt}`
+        }
+    </script>
 
