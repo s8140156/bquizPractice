ad9964edd39d5f572a27b871bcb47856d943ab24
s8140156 <s8140156@gmail.com>
2024-03-09 16:59:30 +0800

    完成人氣文章邏輯 step16

--- bz2-4/front/pop.php
+++ bz2-4/front/pop.php
@@ -1,10 +1,10 @@
 <fieldset>
-    <legend>目前位置：首頁 > 最新文章區</legend>
+    <legend>目前位置：首頁 > 人氣文章區</legend>
     <table style="width:90%;margin:auto">
         <tr>
             <th width="30%">標題</th>
             <th width="50%">內容</th>
-            <th width="20%"></th>
+            <th width="20%">人氣</th>
         </tr>
         <?php
         $total=$News->count(['sh'=>1]);
@@ -12,16 +12,20 @@
         $pages=ceil($total/$div);
         $now=$_GET['p']??1;
         $start=($now-1)*$div;
-        $rows=$News->all(['sh'=>1]," limit $start,$div");
+        $rows=$News->all(['sh'=>1]," order by `good` desc limit $start,$div");
         foreach($rows as $row){
         ?>
         <tr>
-            <td class="title clo" data-id="<?=$row['id'];?>" style="cursor:pointer"><?=$row['title'];?></td>
+            <td class="title clo" data-id="<?=$row['id'];?>"><?=$row['title'];?></td>
             <td>
-                <div id="s<?=$row['id'];?>"><?=mb_substr($row['news'],0,20);?>...</div> 
-                <div id="a<?=$row['id'];?>" style="display:none"><?=$row['news'];?></div>
+                <div style="position:relative"><?=mb_substr($row['news'],0,20);?>...</div> 
+                <div id="p<?=$row['id'];?>" class="pop">
+                    <h3 style="color:lightblue"><?=$row['title'];?></h3>
+                    <pre> <?=$row['news'];?></pre>
+               </div>
             </td>
             <td>
+            <span><?=$row['good'];?></span>個人說<img src="./icon/02B03.jpg" style="width:25px">
                 <?php
                 if(isset($_SESSION['user'])){
                     if($Log->count(['news'=>$row['id'],'acc'=>$_SESSION['user']])>0){
@@ -42,23 +46,24 @@
         <?php
         if($now-1>0){
             $prev=$now-1;
-            echo "<a href='index.php?do=news&p=$prev'> < </a>";
+            echo "<a href='index.php?do=pop&p=$prev'> < </a>";
         }
         for($i=1;$i<=$pages;$i++){
             $size=($now==$i)?'font-size:22px':'font-size:16px';
-            echo "<a href='index.php?do=news&p=$i' style='$size'> $i </a>";
+            echo "<a href='index.php?do=pop&p=$i' style='$size'> $i </a>";
         }
         if($now+1<=$pages){
             $next=$now+1;
-            echo "<a href='index.php?do=news&p=$next' style='$size'> > </a>";
+            echo "<a href='index.php?do=pop&p=$next' style='$size'> > </a>";
         }
         ?>
     </div>
 </fieldset>
 <script>
-    $('.title').on('click',function(){
+    $('.title').hover(function(){
+        $('.pop').hide()
         let id=$(this).data('id')
-        $('#s'+id).toggle()
-        $('#a'+id).toggle()
+        $('#p'+id).show()
     })
+
 </script>
--- bz2-4/index.php
+++ bz2-4/index.php
@@ -1,4 +1,16 @@
-﻿<?php include_once "./api/db.php" ?>
+﻿<style>
+	.pop{
+		background:rgba(51,51,51,0.8); 
+		color:#FFF; 
+		min-height:100px; 
+		width:300px; 
+		position:absolute; 
+		display:none; 
+		z-index:9999; 
+		overflow:auto;
+	}
+</style>
+<?php include_once "./api/db.php" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
