68c64097098f366c2bad637b5cacdfdeadd14972
s8140156 <s8140156@gmail.com>
2024-03-11 19:15:27 +0800

    完成後台帳號管理文章管理功能

---
+++ bz2-6/api/edit_news.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once "db.php";
+
+if(isset($_POST['id'])){
+    foreach($_POST['id'] as $id){
+        if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+            $News->del($id);
+        }else{
+            $rows=$News->find($id);
+            $rows['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+            $News->save($rows);
+        }
+    }
+}
+
+to("../back.php?do=news");
+
+
+?>
