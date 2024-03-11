9a038c11e35b16d47e6707dee610981691428a45
s8140156 <s8140156@gmail.com>
2024-03-11 13:04:13 +0800

    開始題組二盲寫第二次

---
+++ bz2-6/test.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once "./api/db.php";
+
+// $Alpha->save(['total'=>'新增','sh'=>1]);
+// $Alpha->save(['id'=>2,'total'=>'改寫了喔','sh'=>0]);
+
+// $row=$Alpha->find(3);
+// $row=$Alpha->all();
+// dd($row);
+
+// $row=$Alpha->count('sh');
+// $row=$Alpha->sum('sh');
+// echo $row;
+
+// $Alpha->del(5);
+
+
+
+?>
