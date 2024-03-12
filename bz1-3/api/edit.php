37727db53d2a505fbbbf841e7d91f10c5117caa0
s8140156 <s8140156@gmail.com>
2024-03-12 18:31:54 +0800

    完成編輯api及上傳圖片api 目前測試ok

---
+++ bz1-3/api/edit.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once "db.php";
+
+$table=$_POST['table'];
+$DB=${ucfirst($table)};
+
+    foreach($_POST['id'] as $key=>$id){
+        if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+            $DB->del($id);
+        }else{
+            $row=$DB->find($id);
+            if(isset($row['text'])){
+                $row['text']=$_POST['text'][$key];
+            }
+            switch($table){
+                case "admin";
+                $row['acc']=$_POST['acc'][$key];
+                $row['pw']=$_POST['pw'][$key];
+                break;
+                case "title";
+                $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
+                break;
+                case "menu";
+                $row['href']=$_POST['href'][$key];
+                $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['del']))?1:0;
+                break;
+                default:
+                $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['del']))?1:0;
+            }
+            $DB->save($row);
+        }
+    }
+
+
+to("../back.php?do=$table");
+
+
+?>
