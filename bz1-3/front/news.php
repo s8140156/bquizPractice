febe0d0f99a62c15da2de831199337a2826bc46a
s8140156 <s8140156@gmail.com>
2024-03-13 11:47:09 +0800

    完成更多消息及校園映像顯示

--- bz1-3/front/news.php
+++ bz1-3/front/news.php
@@ -3,9 +3,43 @@
 				<?php include "marquee.php"; ?>
 				<div style="height:32px; display:block;"></div>
 				<!--正中央-->
-				<div style="text-align:center;">
-					<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-					<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
-				</div>
+				<h3 class="cent">更多最新消息顯示區</h3>
+				<hr>
+				<?php
+				$total=$News->count(['sh'=>1]);
+				$div=5;
+				$pages=ceil($total/$div);
+				$now=$_GET['p']??1;
+				$start=($now-1)*$div;
+				$rows=$News->all(['sh'=>1]," limit $start,$div");
+				?>
+				<ol start="<?=$start+1;?>">
+				<?php
+				foreach($rows as $row){
+				echo "<li class='sswww'>";
+				echo mb_substr($row['text'],0,20);
+				echo "<div class='all' style='display:none'>";
+				echo $row['text'];
+				echo "</div>";
+				echo "...</li>";
+				}
+				?>
+				</ol>
+				<div class="cent">
+			<?php
+			if($now-1>0){
+				$prev=$now-1;
+				echo "<a href='?do=$do&p=$prev'> < </a>";
+			}
+			for($i=1;$i<=$pages;$i++){
+				$size=($i==$now)?'font-size:22px':'font-size:16px';
+				echo "<a href='?do=$do&p=$i' style='$size'> $i </a>";
+			}
+			if($now+1<=$pages){
+				$next=$now+1;
+				echo "<a href='?do=$do&p=$next'> > </a>";
+			}
+			?>
+		</div>
 			</div>
 			<!-- news di end -->
