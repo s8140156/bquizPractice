367859a5da3e83d1e5619a30e694dd5268d3a145
s8140156 <s8140156@gmail.com>
2024-03-21 22:03:00 +0800

    建置後台預告片新增畫面與寫入邏輯及管理畫面呈現

---
+++ bz3-1/api/add_poster.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "db.php";
+
+if(isset($_FILES['poster']['tmp_name'])){ //誒前面欄位name是取poster 所以當然帶進來是['poster']...
+    move_uploaded_file($_FILES['poster']['tmp_name'],"../img/".$_FILES['poster']['name']);
+    $_POST['img']=$_FILES['poster']['name'];
+}
+$_POST['sh']=1;
+$_POST['rank']=$Poster->max('id')+1;
+$_POST['ani']=rand(1,3);
+
+$Poster->save($_POST);
+to("../back.php?do=poster");
+
+
+
+
+?>
