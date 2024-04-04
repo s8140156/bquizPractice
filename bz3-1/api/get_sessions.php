553fcdb2fff5a14b244df887f1184753550d61cc
s8140156 <s8140156@gmail.com>
2024-04-04 19:12:00 +0800

    依當下時間計算剩餘場次

--- bz3-1/api/get_sessions.php
+++ bz3-1/api/get_sessions.php
@@ -4,8 +4,10 @@ include_once "db.php";
 
 $movie=$_GET['movie'];
 $date=$_GET['date'];
+$H=date("G"); //算一天多少小時 使用G(不補零的24小時格式 計算可使用)
+$start=($H<14)?1:6-(ceil((24-$H)/2)-1);
 
-for($i=1;$i<=5;$i++){
+for($i=$start;$i<=5;$i++){
     echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
     // echo "<option value='$i'>$i</option>"; //兩種寫法都可以
 }
