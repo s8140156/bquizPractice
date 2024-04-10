6efec48113761297e538ddc6631109ccb7c2220b
s8140156 <s8140156@gmail.com>
2024-04-10 22:58:26 +0800

    刪除不必要程式 完成題三理解及練習

--- bz3-1/api/qdel.php
+++ bz3-1/api/qdel.php
@@ -5,7 +5,6 @@ include_once "db.php";
 // $Order->del(["{$_POST['type']}"=>$_POST['val']]);
 $data[$_POST['type']]=$_POST['val']; //key是type value是val
 $Order->del($data);
-// $DB->del($_POST['id']);
 
 
 ?>
